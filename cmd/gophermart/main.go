e45120d87ad051f975c4e3294f617f3c85693a04
Victor N. Skurikhin <vs@lnhost.net>
2024-04-11 17:57:33 +0300

    Close #10. Infrastructure code.

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,14 @@
+/*
+ * This file was last modified at 2024-04-11 18:20 by Victor N. Skurikhin.
+ * main.go
+ * $Id$
+ */
+
 package main
 
-func main() {}
+func main() {
+	//goland:noinspection GoInfiniteFor
+	for {
+
+	}
+}
