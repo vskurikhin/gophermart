f63334e8b0e1c5a931846879519fd1f847f84c67
Victor N. Skurikhin <vs@lnhost.net>
2024-04-13 18:30:28 +0300

    Close #17. Zap logger configured.

--- go.mod
+++ go.mod
@@ -18,6 +18,8 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- internal/env/config.go
+++ internal/env/config.go
@@ -1,12 +1,15 @@
 /*
- * This file was last modified at 2024-04-13 17:19 by Victor N. Skurikhin.
+ * This file was last modified at 2024-04-13 19:08 by Victor N. Skurikhin.
  * config.go
  * $Id$
  */
 
 package env
 
-import "sync"
+import (
+	"github.com/vskurikhin/gophermart/internal/logger"
+	"sync"
+)
 
 type Config struct {
 	accrualSystemAddress string
@@ -22,33 +25,35 @@ func GetConfig() *Config {
 
 	once.Do(func() {
 		config = new(Config)
+		e := newEnvironments()
+		f := newFlags()
+
+		if e.AccrualSystemAddress() != "" {
+			config.accrualSystemAddress = e.AccrualSystemAddress()
+		} else {
+			config.accrualSystemAddress = *f.AccrualSystemAddress()
+		}
+
+		if e.Address() != "" {
+			config.address = e.Address()
+		} else {
+			config.address = *f.Address()
+		}
+
+		if e.DataBaseDSN() != "" {
+			config.dataBaseDSN = e.DataBaseDSN()
+		} else {
+			config.dataBaseDSN = *f.DataBaseDSN()
+		}
+
+		if e.Key() != "" {
+			config.key = e.Key()
+		} else {
+			config.key = *f.Key()
+		}
+		logger.Development = *f.DevelopmentLogger()
 	})
-	e := newEnvironments()
-	f := newFlags()
 
-	if e.AccrualSystemAddress() != "" {
-		config.accrualSystemAddress = e.AccrualSystemAddress()
-	} else {
-		config.accrualSystemAddress = *f.AccrualSystemAddress()
-	}
-
-	if e.Address() != "" {
-		config.address = e.Address()
-	} else {
-		config.address = *f.Address()
-	}
-
-	if e.DataBaseDSN() != "" {
-		config.dataBaseDSN = e.DataBaseDSN()
-	} else {
-		config.dataBaseDSN = *f.DataBaseDSN()
-	}
-
-	if e.Key() != "" {
-		config.key = e.Key()
-	} else {
-		config.key = *f.Key()
-	}
 	return config
 }
 
